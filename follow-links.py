8831d378c1facee64a66870c126387941ad754a0
ivysam-cmyk <98635284+ivysam-cmyk@users.noreply.github.com>
2023-01-04 21:02:04 +0800

    Code replaced to allow 'tags' to update

--- follow-links.py
+++ follow-links.py
@@ -8,31 +8,40 @@ ctx.verify_mode = ssl.CERT_NONE
 
 # Use link as string
 # First name is in the link
-url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
+url = "http://py4e-data.dr-chuck.net/known_by_Eassan.html"
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-# Initial position is 1
-position = 1
-# the number of times a link is visited
+# the loop restarts when another link is visited
+boolean = True
 link_visited = 0
-for tag in tags:
-  link = tag.get('href', None)
-  if (position == 3):
-    name = tag.contents[0] 
-    print(name)
-    link_visited += 1
-    if (link_visited == 4):
-      # At the 4th time that the link is clicked record the name and break
+# stop visiting more links upon reaching the 7th person
+
+redirect_limit = int(input("Enter count: "))
+tag_limit = int(input("Enter position: "))
+
+while boolean == True:
+# Initial position is 1
+  tag_position = 1
+  tags = soup('a')
+# the number of times a link is visited
+  for tag in tags:
+    link = tag.get('href', None)
+    if (tag_position == tag_limit):
       name = tag.contents[0] 
       print(name)
-      break
-    url = link
-    html = urllib.request.urlopen(url, context=ctx).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    # reset the position so that it can be used again after opening the url
-    position = 1
+      link_visited += 1
+      if (link_visited == redirect_limit):
+        boolean = False
+        # if the above doesn't stop the loop, then...
+        break
+      url = link
+      html = urllib.request.urlopen(url, context=ctx).read()
+      soup = BeautifulSoup(html, 'html.parser')
+      # need to change the tags variable, include it in a loop
+      tags = soup('a')
+      # tags does change but the tags used in 'for' loop does not change
+      # reset the tag_position so that it can be used again after opening the url
 
-  position += 1
+    tag_position += 1
